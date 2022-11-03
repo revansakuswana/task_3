3355b7d1aa24cc8e8812f2f235cc6444a825bcb9
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2022-11-03 19:26:41 +0700

    commit

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const tentang = document.getElementById('tentang')
 tentang.innerHTML = 'About Myself (menggunakan javascript)'
 
-let newElement = document.createElement('p');
-newElement.innerHTML = 'skill (menggunakan javascript)';
-console.log(newElement);
-let newClass = document.querySelector('#skill');
-newClass.appendChild(newElement);
+var Skill = document.querySelector('#skill');
+var newElemen = document.createElement("h4");
+var teksElemen = document.createTextNode('Skill (menggunakan javascript)');
+newElemen.appendChild(teksElemen);
+skill.appendChild(newElemen);
 
-function movin(obj) {
-    obj.innerHTML = "Taraaa makjreng"
-  }  
-function movout(obj) {
-    obj.innerHTML = "Arahkan Kursor Kesini"
+function mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+    obj.innerHTML = "Release Me";
+  }
+  
+  function mUp(obj) {
+    obj.style.backgroundColor="#D94A38";
+    obj.innerHTML="Terima Kasih";
   }
