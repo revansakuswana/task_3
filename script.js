6458f4339bbe717d8f05fd38120955938721f003
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2022-11-03 16:09:53 +0700

    commit

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-//No 1
-let changeText = document.querySelector('#tentang');
-changeText.innerText = 'About Myself';
+const tentang = document.getElementById('tentang')
+tentang.innerHTML = 'About Myself (menggunakan javascript)'
 
-//No 2
 let newElement = document.createElement('p');
 newElement.innerHTML = 'skill (menggunakan javascript)';
 console.log(newElement);
 let newClass = document.querySelector('#skill');
 newClass.appendChild(newElement);
 
-//No 3
-function ubahbg() {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-}
+function movin(obj) {
+    obj.innerHTML = "Taraaa makjreng"
+  }  
+function movout(obj) {
+    obj.innerHTML = "Arahkan Kursor Kesini"
+  }
